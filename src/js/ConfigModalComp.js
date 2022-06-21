58b362cb1cf243f65b67f7343590380bdeedfbc8
Atomie CHEN <atomic_cwh@163.com>
2022-06-22 00:45:38 +0800

    Minor: change modal name from 设置对照名单 to 设置名单

--- src/js/ConfigModalComp.js
+++ src/js/ConfigModalComp.js
@@ -299,7 +299,7 @@ export default {
         <div class="modal-background"></div>
         <div class="modal-card">
         <header class="modal-card-head">
-            <p class="modal-card-title">设置对照名单</p>
+            <p class="modal-card-title">设置名单</p>
             <button class="delete" aria-label="close"></button>
         </header>
         <section class="modal-card-body">
